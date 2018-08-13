09a1da732004bb566dcf4a5e2357973bd993eb34
Man Of Code <shreeda@Shreedas-MacBook-Pro.local>
2018-08-13 21:51:47 +0530

    HourGlasses

---
+++ java-basic-coding/src/main/java/com/shreeda/example/javabasiccoding/HourGlasses.java
@@ -0,0 +1,53 @@
+package com.shreeda.example.javabasiccoding;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class HourGlasses {
+
+    static Integer max = null;
+    // Complete the hourglassSum function below.
+    static int hourglassSum(int[][] arr) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                Integer sum = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2];
+                if (max == null) {
+                    max = new Integer(sum);
+                    continue;
+                }
+                if (sum > max) {
+                    max = sum;
+                }
+            }
+        }
+        return max;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+
+        int[][] arr = new int[6][6];
+
+        for (int i = 0; i < 6; i++) {
+            String[] arrRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 6; j++) {
+                int arrItem = Integer.parseInt(arrRowItems[j]);
+                arr[i][j] = arrItem;
+            }
+        }
+
+        int result = hourglassSum(arr);
+
+        System.out.println(result);
+
+        scanner.close();
+    }
+}
